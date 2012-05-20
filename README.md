4af9b3c094f032a175c9c37d1c7f0540f40734c3
René Meye <rene@meye.md>
2012-05-20 22:36:16 +0200

    Created empty rails app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-messageboard
+Messageboard
 ============
 
 An easy to use Software to display news, advertisements and other Information on public screens (POS POI).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messageboard::Application.config.secret_token = '0b92747747168ab87cad5135269b3e94074da8f517f918d9afdd5a986e29a2e97fa9fdacf08bfaf3d75035105a46ecb7c17d8d04a95569f24ca9e447189b71db'
